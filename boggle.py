8faac7b4ad6f041a6eb98e3f9dcc0de906105fc1
Ana Guerrero <amguerreron@gmail.com>
2021-11-01 19:49:45 -0500

    First challenges

---
+++ boggle.py
@@ -0,0 +1,106 @@
+
+import string
+import random
+
+
+def main():
+    '''This is the main function that gets executed when calling boggle.py'''
+    print("Hello World!")
+
+def greet_user(name):
+    '''Greet the user'''
+    '''name arg of the user'''
+    print(f'Guatsup {name}')
+
+def generate_dummy_board():
+    '''Generate a dummy board'''
+    print(' ----- ----- ----- -----')
+    print('|  A  |  I  |  N  |  Q  |')
+    print('----- ----- ----- -----')
+    print ('|  B  |  P  |  U  |  R  |')
+    print('----- ----- ----- -----')
+    print('|  T  |  M  |  A  |  E  |')
+    print('----- ----- ----- -----')
+    print('|  X  |  G  |  U  |  D  | ')
+    print('----- ----- ----- -----')
+
+def generate_random_board():
+    '''Generate a random board'''
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    print(random.choice(letters))
+    print(' ----- ----- ----- -----')
+    print(f'|  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |')
+    print(' ----- ----- ----- -----')
+    print(f'|  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |')
+    print(' ----- ----- ----- -----')
+    print(f'|  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |')
+    print(' ----- ----- ----- -----')
+    print(f'|  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |')
+    print(' ----- ----- ----- -----')   
+
+def generate_random_board_loop_row():
+    '''Generate a random board'''
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    for i in range(1,5):
+        print(' ----- ----- ----- -----')
+        print(f'|  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |  {random.choice(letters)}  |')
+    print(' ----- ----- ----- -----')
+
+def generate_random_board_loop():
+    '''Generate a random board'''
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    for i in range(1,5):
+        print(' ----- ----- ----- -----')
+        row = ''
+        for col in range (1,5):
+            row = row + f'|  {random.choice(letters)}  ' 
+        row = row + '|'
+        print(row)
+    print(' ----- ----- ----- -----')
+       
+    
+def random_dice_list_first():
+    '''Get random list of 16 dices with valid random letters'''
+    dices = ['ARHSDE','FUAARB','IOTALG','UOEEOC','FOMTUI','OODBLG','RPSZTL','EBIOUA','CAREME','RSJEFI','NSXJAH','UVDQBC','NBIMEE','BAANIT','EPVOCU','SCAAPT']
+    random.shuffle(dices)
+    for r in range(1,5):
+        print(' ----- ----- ----- -----')        
+        row = ''
+        for col in range (1,5):
+            diceNumber = int((r-1)*4+col-1)
+            row = row + f'|  {random.choice(dices[diceNumber])}  '
+        row = row + '|'
+        print(row)
+        
+    print(' ----- ----- ----- -----')
+
+def random_dice_list():
+    '''Get random list of 16 dices with valid random letters'''
+    dices = ['ARHSDE','FUAARB','IOTALG','UOEEOC','FOMTUI','OODBLG','RPSZTL','EBIOUA','CAREME','RSJEFI','NSXJAH','UVDQBC','NBIMEE','BAANIT','EPVOCU','SCAAPT']
+    random.shuffle(dices)
+    for r in range(1,5):
+        print(' ----- ----- ----- -----')        
+        row = ''
+        for col in range (1,5):
+            # Use dices as a bag, and remove each time one dice from the bag
+            # ['AB', 'CD'].pop() --> returns 'CD' and the list is now only ['AB']
+            row = row + f'|  {random.choice(dices.pop())}  '
+        row = row + '|'
+        print(row)
+        
+    print(' ----- ----- ----- -----')
+
+
+
+if __name__ == "__main__":
+    #main()
+    greet_user('Ana' + ' Guerro')
+    #generate_random_board()
+    # generate_random_board_loop()
+    random_dice_list()
+
+
+
+    
